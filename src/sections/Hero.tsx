4284b5fd7d2144fbd61498a369ea6605d3958036
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-16 13:24:38 -0600

    Added radial gradient overlay to the entire globe section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,10 +4,13 @@ import starsBg from '@/assets/stars.png';
 
 export const Hero = () => {
   return (
-    <section className="h-[492px] flex items-center overflow-hidden relative" 
+    <section className="h-[492px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]" 
       style={{
       backgroundImage: `url(${starsBg.src})`
       }}>
+    <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgba(140,69,255,0.5)_15%,rgba(14,0,36,0.5)_78%,transparent)]">
+    </div>
+
        <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,0.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]">
         </div>
         <div className="absolute h-[344px] w-[344px] border  border-white opacity-20 rounded-full top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 ">
