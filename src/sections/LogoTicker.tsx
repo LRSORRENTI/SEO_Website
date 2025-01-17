099ff2f8930e4e622721266334370e5d9bb02a79
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-17 08:13:04 -0600

    Added setup for logo ticker section, also fixed backdrop blur on header component

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,34 @@
+import acmeLogo from '@/assets/logo-acme.png';
+import apexLogo from '@/assets/logo-apex.png';
+import celestial from '@/assets/logo-celestial.png';
+import quantum from '@/assets/logo-quantum.png';
+import pulse from '@/assets/logo-pulse.png';
+import echo from '@/assets/logo-echo.png';
+
 export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
+  const logos = [acmeLogo, apexLogo, celestial, quantum, pulse, echo];
+
+  return (
+    <section className='py-20 md:py-24'>
+      <div className="container">
+        <div className='flex items-center gap-5'>
+          <div className='flex-1 md:flex-none'>
+          <h2>Trusted by top innovative teams</h2>
+          </div>
+          <div className='flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
+          <div className='flex flex-none gap-16'>
+            {logos.map((logo, index) => (
+              <img
+                src={logo.src} 
+                key={index} 
+                alt={`Logo ${index + 1}`} 
+                className="h-6 w-auto "
+              />
+            ))}
+          </div>
+         </div>
+        </div>
+      </div>
+    </section>
+  );
 };
