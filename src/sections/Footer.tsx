e94377289c99c14b62b7c39a28681079ac23f9d2
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-18 14:35:08 -0600

    Added footer section markup

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,34 @@
+import Logo from '@/assets/logo.svg';
+import XSocial from '@/assets/social-x.svg';
+import InstaSocial from '@/assets/social-instagram.svg';
+import YTSocial from '@/assets/social-youtube.svg';
+
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className='py-5 border-t border-white/15 '>
+      <div className='container'>
+        <div className='flex flex-col md:flex-row lg:items-center sm:gap-16 lg:gap-8'>
+        <div className='flex gap-2 items-center lg:flex-1'>
+          <Logo className="w-6 h-6" />
+          <div className='font-medium'>AI SEO</div>
+        </div>
+
+          <nav className='flex flex-col md:flex-row gap-5 lg:gap-7 lg:flex-1 lg:justify-center'>
+            <a href="" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Features</a>
+            <a href="" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Developers</a>
+            <a href="" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Company</a>
+            <a href="" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Blog</a>
+            <a href="" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Changelog</a>
+          </nav>
+      
+        <div className='flex gap-5 lg:flex-1 lg:justify-end '>
+          <XSocial     className="text-white/40 hover:text-white transition"   />
+          <InstaSocial className="text-white/40 hover:text-white transition"   />
+          <YTSocial    className="text-white/40 hover:text-white transition"   />
+        </div>
+        </div>
+      </div>
+    </footer>
+  )
 };
