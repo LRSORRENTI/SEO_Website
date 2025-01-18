999535dda8a97eda9277c9b2e0cf10427fe10ba6
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-18 08:32:45 -0600

    added styles for testimonials component

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -33,18 +33,20 @@ const testimonials = [
 
 export const Testimonials = () => {
   return (
-  <section className="py-20 ">
+  <section className="py-20 md:py-24 ">
     <div className="container">
-      <h2 className="text-5xl text-center tracking-tighter font-medium ">Beyond Expectations</h2>
-      <p className="text-white/70 text-lg text-center mt-5 tracking-tight">
+      <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium ">Beyond Expectations</h2>
+      <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">
         Our revolutionary AI SEO tools have transformed 
         our clients' strategies.
       </p>
+      <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+      <div className="flex gap-5">
       {testimonials.map(testimonial => (
           <div
             key={testimonial.name}
-            className="border border-white/15 p-6 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)]">
-            <div className="text-lg tracking-tight">{testimonial.text}</div>
+            className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none ">
+            <div className="text-lg md:text-2xl tracking-tight">{testimonial.text}</div>
            <div className="flex items-center gap-3 mt-5">
            <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,140)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
            <Image 
@@ -55,11 +57,13 @@ export const Testimonials = () => {
               </div>
             <div>
             <div>{testimonial.name}</div>
-            <div>{testimonial.title}</div>
+            <div className="text-white/50 text-sm">{testimonial.title}</div>
             </div>
           </div>
         </div>
       ))}
+      </div>
+      </div>
     </div>
   </section>
   );
