61363870ef4ca5758805e04707bbb2f69df9db88
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-19 15:50:58 -0600

    Added animations to the tabs in features section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -3,7 +3,7 @@
 import { DotLottieCommonPlayer, DotLottiePlayer } from "@dotlottie/react-player";
 import Image from "next/image";
 import productImage from '@/assets/product-image.png';
-import { useEffect, useRef } from "react";
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from "react";
 import { DotLottie } from "@lottiefiles/dotlottie-react";
 import { animate, motion, useMotionTemplate, useMotionValue, ValueAnimationTransition } from "framer-motion";
 
@@ -34,7 +34,9 @@ const tabs = [
   },
 ];
 
-const FeatureTab = (tab: typeof tabs[number]) => {
+const FeatureTab = (props: typeof tabs[number] & ComponentPropsWithoutRef<'div'> &  { selected: boolean }) => {
+
+  const tabRef = useRef<HTMLDivElement>(null);
 
   const dotLottieRef = useRef<DotLottieCommonPlayer>(null);
 
@@ -44,8 +46,21 @@ const FeatureTab = (tab: typeof tabs[number]) => {
   const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}%, black, transparent)`
 
   useEffect(() => {
+    if(!tabRef.current || !props.selected) return;
+
+    xPercentage.set(0);
+    yPercentage.set(0);
+
+    const { height, width } = tabRef.current?.getBoundingClientRect();
+
+    const circumference = height * 2 + width * 2;
+
+    const times = [0, width / circumference,
+                   (width + height) / circumference,
+                   (width * 2 + height) / circumference, 1];
 
     const options: ValueAnimationTransition = {
+      times,
       duration: 4,
       repeat: Infinity,
       ease: 'linear',
@@ -55,7 +70,7 @@ const FeatureTab = (tab: typeof tabs[number]) => {
     animate(xPercentage, [0, 100, 100, 0, 0], options);
     animate(yPercentage, [0, 0, 100, 100, 0], options);
   
-  }, [])
+  }, [props.selected])
 
   const handleTabHover = () => {
     if (dotLottieRef.current === null ) return;
@@ -65,28 +80,37 @@ const FeatureTab = (tab: typeof tabs[number]) => {
 
   return (
     <div 
+      ref={tabRef}
       onMouseEnter={handleTabHover}
+      onClick={props.onClick}
       className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1 relative">
+      {props.selected && (
       <motion.div 
       style={{
         maskImage,
       }}
-      className="absolute inset-0 -m-px border border-[#A369FF] rounded-xl "></motion.div>
+      className="absolute inset-0 -m-px border border-[#A369FF] rounded-xl ">
+      </motion.div>
+      )}
+
       <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
       <DotLottiePlayer 
         ref={dotLottieRef}
-        src={tab.icon} 
+        src={props.icon} 
         className="h-5 w-5"
         autoplay
         />
         </div>
-        <div className="font-medium">{tab.title}</div>
-        {tab.isNew && <div className="text-xs rounded-full px-2 py-0.5 bg-[#8C44FF] text-black font-semibold">New</div> }
+        <div className="font-medium">{props.title}</div>
+        {props.isNew && <div className="text-xs rounded-full px-2 py-0.5 bg-[#8C44FF] text-black font-semibold">New</div> }
         </div>
   )
 }
 
 export const Features = () => {
+
+  const [selectedTab, setSelectedTab] = useState(0);
+
   return <section className="py-20 md:py-24">
     <div className="container">
       <h2 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">Elevate your SEO efforts.</h2>
@@ -95,8 +119,12 @@ export const Features = () => {
         businesses approach SEO.
       </p>
       <div className="mt-10 flex flex-col lg:flex-row gap-3">
-      {tabs.map(tab => (
-        <FeatureTab {...tab} key={tab.title} />
+      {tabs.map((tab, tabIndex) => (
+        <FeatureTab 
+            {...tab} 
+            selected={selectedTab === tabIndex}
+            onClick={() => setSelectedTab(tabIndex)} key={tab.title} 
+            />
       ))} 
       </div>
       <div className="border border-white/20 p-2.5 rounded-xl mt-3">
