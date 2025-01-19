d11f942e5056caddb902c5ebd2eb483698f25966
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-19 09:34:52 -0600

    Added dot lottie player animations to the lottie files

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { DotLottiePlayer } from "@dotlottie/react-player";
+import { DotLottieCommonPlayer, DotLottiePlayer } from "@dotlottie/react-player";
 import Image from "next/image";
 import productImage from '@/assets/product-image.png';
+import { useRef } from "react";
+import { DotLottie } from "@lottiefiles/dotlottie-react";
 
 const tabs = [
   {
@@ -31,6 +33,34 @@ const tabs = [
   },
 ];
 
+const FeatureTab = (tab: typeof tabs[number]) => {
+
+  const dotLottieRef = useRef<DotLottieCommonPlayer>(null);
+
+  const handleTabHover = () => {
+    if (dotLottieRef.current === null ) return;
+    dotLottieRef.current.seek(0);
+    dotLottieRef.current.play();
+  }
+
+  return (
+    <div 
+      onMouseEnter={handleTabHover}
+      className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1">
+      <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
+      <DotLottiePlayer 
+        ref={dotLottieRef}
+        src={tab.icon} 
+        className="h-5 w-5"
+        autoplay
+        />
+        </div>
+        <div className="font-medium">{tab.title}</div>
+        {tab.isNew && <div className="text-xs rounded-full px-2 py-0.5 bg-[#8C44FF] text-black font-semibold">New</div> }
+        </div>
+  )
+}
+
 export const Features = () => {
   return <section className="py-20 md:py-24">
     <div className="container">
@@ -41,20 +71,8 @@ export const Features = () => {
       </p>
       <div className="mt-10 flex flex-col lg:flex-row gap-3">
       {tabs.map(tab => (
-        <div 
-        key={tab.title} 
-        className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1">
-          <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
-          <DotLottiePlayer 
-            src={tab.icon} 
-            className="h-5 w-5"
-            autoplay
-            />
-            </div>
-            <div className="font-medium">{tab.title}</div>
-            {tab.isNew && <div className="text-xs rounded-full px-2 py-0.5 bg-[#8C44FF] text-black font-semibold">New</div> }
-            </div>
-      ))}
+        <FeatureTab {...tab} key={tab.title} />
+      ))} 
       </div>
       <div className="border border-white/20 p-2.5 rounded-xl mt-3">
       <div className="aspect-video bg-cover border border-white/20 rounded-lg" style={{
