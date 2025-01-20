6a2b65944656e05205cbb5124fe65dcbcbaf4c9e
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-20 10:14:57 -0600

    updated hover styles for button, and finished animations for the Call to Action component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 const Button = (props: React.PropsWithChildren) => {
     return (
-    <button className='relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff] hover:text-white/90 transition ease-in-out duration-500 '>
+    <button className='relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff] hover:text-white/80 transition ease-in-out duration-500 '>
     <div className='absolute inset-0'>
     <div className='rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]'></div>
     <div className='rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]'></div>
