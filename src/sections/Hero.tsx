158f3680766cfed22b30e848f8c97218f254e2ff
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-20 11:34:27 -0600

    updated text to use &apos;

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -104,7 +104,7 @@ const backgroundPositionY = useTransform(
 
       <div className="container relative mt-16">
           <h1 className="text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.50))] text-transparent bg-clip-text text-center">AI SEO</h1>
-          <p className="text-lg md:text-xl  text-white/70 mt-5 text-center max-w-xl mx-auto ">Elevate your site's visibility effortlessly 
+          <p className="text-lg md:text-xl  text-white/70 mt-5 text-center max-w-xl mx-auto ">Elevate your site&apos;s visibility effortlessly 
              with AI, where smart technology meets 
              user-friendly SEO tools
           </p>
